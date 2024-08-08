081f5c59261ba36caca679d36718f0b2a199ef73
thonyxclusive <123905984+thonyxclusive@users.noreply.github.com>
2024-08-08 01:59:12 +0100

    inventory

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 'use client'
 import Image from "next/image"
-import {useState, useEffect} from 'react'
-import {firestore} from '@/firebase'
-import {Box, Typography, Stack, Modal, TextField, Button} from '@mui/material'
-import {collection, doc, getDocs, query, setDoc, deleteDoc, getDoc,} from 'firebase/firestore'
+import { useState, useEffect } from 'react'
+import { firestore } from '@/firebase'
+import { Box, Typography, Stack, Modal, TextField, Button } from '@mui/material'
+import { collection, doc, getDocs, query, setDoc, deleteDoc, getDoc } from 'firebase/firestore'
 
 export default function Home() {
   const [inventory, setInventory] = useState([])
@@ -11,45 +11,45 @@ export default function Home() {
   const [itemName, setItemName] = useState('')
 
   const updateInventory = async () => {
-    const snapshot = query (collection(firestore, 'inventory'))
+    const snapshot = query(collection(firestore, 'inventory'))
     const docs = await getDocs(snapshot)
     const inventoryList = []
     docs.forEach((doc) => {
       inventoryList.push({
-        name: doc.id,...doc.data() })
+        name: doc.id,
+        ...doc.data()
+      })
     })
     setInventory(inventoryList)
-    
   }
 
-  const addItem = async (item) =>{
+  const addItem = async (item) => {
     const docRef = doc(collection(firestore, 'inventory'), item)
     const docSnap = await getDoc(docRef)
 
-    if (docSnap.exists()){
-      const {quantity} = docSnap.data()
-      await setDoc(docRef, {quantity: quantity + 1})
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data()
+      await setDoc(docRef, { quantity: quantity + 1 })
     } else {
-        await setDoc(docRef,{quantity: 1})
-      }
-    
-     await updateInventory()
+      await setDoc(docRef, { quantity: 1 })
+    }
+
+    await updateInventory()
   }
 
-  const removeItem = async (item) =>{
+  const removeItem = async (item) => {
     const docRef = doc(collection(firestore, 'inventory'), item)
     const docSnap = await getDoc(docRef)
 
-    if (docSnap.exists()){
-      const {quantity} = docSnap.data()
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data()
       if (quantity === 1) {
         await deleteDoc(docRef)
-      }
-      else {
-        await setDoc(docRef, {quantity: quantity - 1})
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 })
       }
     }
-  
+
     await updateInventory()
   }
 
@@ -57,100 +57,101 @@ export default function Home() {
     updateInventory()
   }, [])
 
-
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
 
   return (
-  <Box 
-    width="100vw"
-    height="100vh"
-    display='flex'
-    justifyContent='center'
-    flexDirection='column'
-    alignItems='center'
-    gap={2}
+    <Box
+      width="100vw"
+      height="100vh"
+      display='flex'
+      justifyContent='center'
+      flexDirection='column'
+      alignItems='center'
+      gap={2}
     >
-    <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description"
-    >
-      <Box
-        position ="absolute"
-        top = "50%"
-        left = "50%"
-        width = {400}
-        bgcolor = "white"
-        border = "2px solid #000"
-        boxShadow = {24}
-        p = {4}
-        display = "flex"
-        flexDirection = "column"
-        gap ={3}
-        sx = {{
-          transform: "translated(-50%, -50%)",
-        }}
-      >
-      <Typography variant="h6">Add Item</Typography>
-      <Stack width = "100%" direction = "row" spacing = {2}>
-        <TextField
-        variant = "outlined"
-        fullWidth
-        value = {itemName}
-        onChange={(e) => {
-          setItemName(e.target.value)
-        }}
-      />
-      <Button variant="outlined" onClick={() => {
-        addItem(itemName)
-        setItemName('')
-        handleClose() 
-      }}
-      >Add</Button>
-      </Stack>
+      <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          width={400}
+          bgcolor="white"
+          border="2px solid #000"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          sx={{
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <Typography variant="h6">Add Item</Typography>
+          <Stack width="100%" direction="row" spacing={2}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => {
+                setItemName(e.target.value)
+              }}
+            />
+            <Button variant="outlined" onClick={() => {
+              addItem(itemName)
+              setItemName('')
+              handleClose()
+            }}
+            >Add</Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Button variant="contained" onClick={handleOpen}>
+        Add New Item
+      </Button>
+      <Box border={'1px solid #333'}>
+        <Box
+          width="800px"
+          height="100px"
+          bgcolor='#ADD8E6'
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+        >
+          <Typography variant='h2' color='#333' textAlign='center'>
+            Inventory Items
+          </Typography>
+        </Box>
+        <Stack width="800px" height="300px" spacing={2} sx={{ overflow: 'auto' }}>
+          {inventory.map(({ name, quantity }) => ( // Added return statement
+            <Box
+              key={name}
+              width="100%"
+              minHeight="150px"
+              display='flex'
+              justifyContent='space-between'
+              alignItems='center'
+              bgColor='#f0f0f0'
+              paddingX={5}
+            >
+              <Typography variant="h3" color="#333" textAlign="center">
+                {name.charAt(0).toUpperCase() + name.slice(1)}
+              </Typography>
+              <Typography variant='h3' color='#333' textAlign='center'>
+                {quantity}
+              </Typography>
+            <Stack direction ="row" spacing={2}>
+              <Button variant="contained" onClick={() => addItem(name)}>
+                Add
+              </Button>
+              <Button variant="contained" onClick={() => removeItem(name)}>
+                Remove
+              </Button>
+            </Stack>
+            </Box>
+          ))}
+        </Stack>
       </Box>
-    </Modal>
-    <Button variant="contained" onClick={handleOpen}>
-      Add New Item
-    </Button>
-    <Box border={'1px solid #333'}>
-      <Box
-        width="800px"
-        height="100px"
-        bgcolor='#ADD8E6'
-        display='flex'
-        justifyContent='center'
-        alignItems='center'
-      >
-       <Typography variant='h2' color='#333' textAlign='center'>
-        Inventory Items
-        </Typography>
-      </Box>  
-      <Stack width="800px" height="300px" spacing={2} overflow='auto'>
-        { inventory.map(({name, quantity}) => {
-          <Box
-          key={name}
-            width="100%"
-            minHeight="150px"
-            display='flex'
-            justifyContent='space-between'
-            alignItems='center'
-            bgcolor='#f0f0f0'
-            paddingX={5}
-          >
-      
-          <Typography variant="h3" color="#333" textAlign="center">
-          {name.charAt(0).toUpperCase() + name.slice(1)}
-          </Typography>
-          <Typography variant='h3' color='#333' textAlign='center'>
-          {quantity}
-          </Typography>
-            <Button variant="contained" onClick={() => removeItem(name)}> 
-              Remove
-            </Button>
-          </Box>
-
-         })}
-      </Stack>
-    </Box> 
-  </Box>
+    </Box>
   )
-}   
+}
